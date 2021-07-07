b08f6fa55260dc0e4a75f9cf8cd75e1049c06095
EdilianiKumquel <kumquelediliani@gmail.com>
2021-07-07 14:52:48 -0430

    improving the code part I

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 let count = 0;
+let countEl = document.getElementById("count-el");
 
 function increment() {
 	count = count + 1;
-	document.getElementById("count-el").innerText = count;
+	countEl.innerText = count;
 }
 
 function reset() {
 	count = 0;
-	document.getElementById('count-el').innerText = count;
+	countEl.innerText = count;
 }
+
