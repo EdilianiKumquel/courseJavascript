d3393504c28281b6afc06416573aa2d3a3007231
EdilianiKumquel <kumquelediliani@gmail.com>
2021-07-06 23:07:57 -0430

    progress about course, section 1, lesson 8

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let count = 0;
+
+var bonusPoints = 50;
+
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints);
+bonusPoints = bonusPoints - 75;
+console.log(bonusPoints);
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
+
+// section 1: build a passenger counter app
+// lesson 8 the onclick event listener 21:40 minutes
