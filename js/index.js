59201075add5759304034069a2eed87da96809f6
EdilianiKumquel <kumquelediliani@gmail.com>
2021-07-08 14:46:59 -0430

    first JS app finished

--- js/index.js
+++ js/index.js
@@ -6,16 +6,11 @@ console.log(countl);
 
 function increment() {
 	count += 1;
-	countEl.innerText = count;
+	countEl.textContent = count;
 }
 function save(){
-	saveEl.innerText += count + " - ";
-}
-
-function reset() {
+	saveEl.textContent += count + " - ";
 	count = 0;
-	countEl.innerText = count;
+	countEl.textContent = count;
 }
 
-//section 1: Build a passenger counter app
-//Lesson 26: Debugging online 1:05:37
