e873b459aff1796d4d5eda32abadb0f828835549
EdilianiKumquel <kumquelediliani@gmail.com>
2021-07-09 21:42:54 -0430

    making a super simple calculator

--- js/index.js
+++ js/index.js
@@ -16,9 +16,34 @@ function save(){
 //SECTION 2 practice time
 
 let error = document.getElementById("error");
-console.log(error);
 
 function error1(){
 	let message = "Something went wrong, please try again";
 	error.innerText = message;
+}
+
+//calculator super simple
+
+let num1 = 8;
+let num2 = 2;
+
+document.getElementById("num1-el").innerText = num1;
+document.getElementById("num2-el").innerText = num2;
+let total = document.getElementById("sum-el");
+let msuma = document.getElementById("sum-el").textContent;
+
+function add(){
+	total.textContent = msuma + (num1 + num2);
+}
+
+function subtract(){
+	total.textContent = msuma + (num1 - num2);
+}
+
+function divide(){
+	total.textContent = msuma + (num1 / num2);
+}
+
+function multiply(){
+	total.textContent = msuma + (num1 * num2);
 }
